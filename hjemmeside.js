0e4369194c23a762c88c2b17bf1e19f7b4b641b4
amalievind <119939287+amalievind@users.noreply.github.com>
2023-02-21 19:56:41 +0100

    galleri js

---
+++ hjemmeside.js
@@ -0,0 +1,19 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("billedeslide");
+  let dots = document.getElementsByClassName("prik");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // skift ved 4 sek
+}
