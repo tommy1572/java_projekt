2779aee762645ae05ac880ba95db2023aa0a95b0
jaco8073 <119938238+jaco8073@users.noreply.github.com>
2023-02-22 10:50:16 +0100

    min knap

--- hjemmeside.js
+++ hjemmeside.js
@@ -23,3 +23,24 @@ function changeColor(element, color) {
   element.style.backgroundColor = color; 
 }
 // slideshow slut
+// Sprog selector starter
+select_element = document.getElementById("langSelect");
+let sprog = ["Dansk", "English", "Deutsch",];
+
+
+// Udvider option-boks baseret på antal sprog i array'et
+for (let i=0; i < sprog.length; i++) {
+let opt = sprog[i];
+select_element.innerHTML += "<option value=\" "+ opt +"\">"+ opt + "</option";
+}
+
+document.getElementById("langSelect").onchange = function(){
+if (document.getElementById("langSelect").value == ' Dansk')
+location.replace('index.html');
+else if (document.getElementById("langSelect").value == ' English')
+location.replace('indexEn.html');
+else
+location.replace('indexDe.html')
+}
+document.getElementById("langSelect").selectedIndex = 1;
+//Sprog selector slut
