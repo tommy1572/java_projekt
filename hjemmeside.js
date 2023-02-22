f97e00c9f862f968c6c56e74c6044e25fcd3fff9
amalievind <119939287+amalievind@users.noreply.github.com>
2023-02-22 11:52:53 +0100

    Update hjemmeside.js

--- hjemmeside.js
+++ hjemmeside.js
@@ -1,22 +1,22 @@
 // slideshow
-let slideIndex = 0;
-showSlides();
+let slideShow = 0;
+kørSlide();
 
-function showSlides() {
+function kørSlide() {
   let i;
   let slides = document.getElementsByClassName("billedeslide");
-  let dots = document.getElementsByClassName("prik");
+  let prikker = document.getElementsByClassName("prik");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  slideShow++;
+  if (slideShow > slides.length) {slideShow = 1}    
+  for (i = 0; i < prikker.length; i++) {
+    prikker[i].className = prikker[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4000); // skift ved 4 sek
+  slides[slideShow-1].style.display = "block";  
+  prikker[slideShow-1].className += " active";
+  setTimeout(kørSlide, 4000); // skift ved 4 sek
 }
 
 function changeColor(element, color) {
